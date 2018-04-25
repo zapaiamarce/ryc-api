9d72364469a30e0a1e58ad8b6140939a4f6250dc
Marcelo Zapaia <mzapaia@ints-MacBook-Pro.local>
2018-04-25 01:16:15 -0300

    auth

--- src/auth.js
+++ src/auth.js
@@ -6,9 +6,12 @@ export default async event => {
   const client = fromEvent(event);
   const api = client.api('simple/v1');
 
-  const { email } = event.data;
-  sgMail.setApiKey(SENDGRID_API_KEY);
+  const { 
+    email,
+    dev,
+  } = event.data;
 
+  sgMail.setApiKey(SENDGRID_API_KEY);
 
   const result = await api.request(`
     {
@@ -21,14 +24,30 @@ export default async event => {
   const userId = result.User.id;
 
   const token = await client.generateAuthToken(userId, 'User');
-
+  const linkHostname = dev ? 'http://localhost:8793/' : 'http://www.ricoycasero.com/';
 
   const msg = {
     to: email,
     from: 'zapaiamarce@gmail.com',
-    subject: 'Your R&C Token',
+    subject: 'Tu cuenta de Rico y Casero',
     // text: 'and easy to do anywhere, even with Node.js',
-    html: `<strong>Your token: ${token}</strong>`,
+    html: `
+        <h1>
+          Click en el siguiente link para ingresar:
+        </h1>
+        <a 
+          style="
+            display:inline-block; 
+            color:white; 
+            background-color:blue; 
+            padding:20px;
+          " 
+          href="${linkHostname}auth?token=${token}"
+        >
+          Ingresar a tu cuenta
+        </a>
+      
+    `,
   };
 
   await sgMail.send(msg);
