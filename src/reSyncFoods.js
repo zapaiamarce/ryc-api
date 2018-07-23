21000c090cf029cd2eeb2d18a0845b3d05dc4389
Marcelo Zapaia <mzapaia@ints-MacBook-Pro.local>
2018-07-23 01:19:36 -0300

    resync foods

---
+++ src/reSyncFoods.js
@@ -0,0 +1,40 @@
+const algoliasearch = require('algoliasearch');
+import { fromEvent } from 'graphcool-lib';
+
+const {
+  ALGOLIA_APP_ID,
+  ALGOLIA_API_KEY,
+  ALGOLIA_INDEX_NAME,
+} = process.env;
+
+const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY)
+const index = client.initIndex(ALGOLIA_INDEX_NAME)
+
+export default async event => {
+  const client = fromEvent(event);
+  const api = client.api('simple/v1');
+
+  await index.clearIndex();
+  
+  const { allFoods } = await api.request(`
+    {
+      allFoods{
+        objectID: id
+        addressstr
+        title
+        _geoloc: location
+      }
+    }
+  `);
+
+  console.log(allFoods);
+  
+  allFoods.forEach(f => index.addObject(f))
+
+  return {
+    data:{
+      response: "ok"
+    }
+  }
+
+}
